9eb87bfbe24052273853cf92cf059a3862a3f7b3
Fixishi <filip.olic01gmail.com>
2022-12-01 09:10:48 +0100

    nn

--- js.js
+++ js.js
@@ -1,17 +1,28 @@
 const guessNumber = document.getElementById('guessNumber');
+const helpNumber = document.getElementById('helpNumber');
     const number = document.getElementById('number');
     const guessBtn = document.getElementById('guessBtn');
     const message = document.getElementById('message');
+    const message2 = document.getElementById('message2');
 
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
     console.log(randomNumber);
 
     guessBtn.addEventListener('click', () => {
         if (number.value == randomNumber) {
             message.textContent = 'You guessed the number!';
         } else if (number.value > randomNumber) {
-            message.textContent = 'Too high!';
+            message.textContent = 'Too high! THE NUMBER IS ' + randomNumber;
+            document.getElementById("guessBtn").disabled = true;
+
         } else {
-            message.textContent = 'Too low!';
+            message.textContent = 'Too low! THE NUMBER IS ' + randomNumber;
+            document.getElementById("guessBtn").disabled = true;
         }
     });
+
+    guessBtn2.addEventListener('click', () => {
+        message2.textContent += "Modulo is:" + ' '+ randomNumber % helpNumber.value;
+        console.log(randomNumber, helpNumber.value);
+    });
+ 
